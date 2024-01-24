cc1d821efc2ff531b45ff48066d584503670fe9e
roboseb <sebastienapril@rocketmail.com>
2024-01-23 18:03:37 -0600

    Update layout to full page and add spacebar input for randomizing.

---
+++ README.md
@@ -0,0 +1,13 @@
+--- To-Do ---
+
+Add option for gaps between beams at specific intervals
+Add buttons for bezier options
+Add colour options
+Add random/procedural options
+multiple stacked canvases
+option to oscillate by animating certain options back and forth between particular values
+Fix issue with one line 
+random delay
+extra parameter options for set line dash
+kaleidoscope / mirrored options/ fractals
+procedural options for randomly changing/animating its own settings
--- script.js
+++ script.js
@@ -7,48 +7,84 @@ let options = {
     lineCount: 20,
     animationSpeed: 5,
     lineLength: 400,
+    lineGap: 800,
+    totalLength: 500,
     lineWidth: 8,
     lineCap: 'round',
-    lineCount: 10,
     angleSize: 180,
+    bezierAngleX: 0,
+    bezierAngleY: 0,
+    startX: 0,
+    startY: 0,
+    lineDelay: 0,
+    subtractedLines: 0,
+}
+
+// Set ranges for randomizing values.
+let ranges = {
+    lineCount: [1, 100],
+    animationSpeed: [-20, 20],
+    lineLength: [1, 1000],
+    lineGap: [1, 1000],
+    totalLength: [50, 2000],
+    lineWidth: [1, 25],
+    angleSize: [-1080, 1080],
+    bezierAngleX: [-1080, 1080],
+    bezierAngleY: [-1080, 1080],
+    startX: [-200, 200],
+    startY: [-200, 200],
+    lineDelay: [-100, 100],
 }
 
 // Canvas and animation initialization based on options.
 let offset = 0;
-ctx.canvas.width = 500;
-ctx.canvas.height = 500;
-ctx.setLineDash([options.lineLength, options.lineLength * 2]);
+ctx.canvas.width = window.innerHeight - 20;
+ctx.canvas.height = window.innerHeight - 20;
+ctx.setLineDash([options.lineLength, options.lineGap]);
 
 ctx.lineWidth = options.lineWidth;
 ctx.lineCap = options.lineCap;
 
-// Draw a line from the center point towards the passed angle.
-function draw(angle) {
+let direction = -5;
+
+// Temp animation function.
+const progressAni = () => {
+    options.bezierAngleX += direction;
+    options.bezierAngleY += direction;
+    if (options.bezierAngleX >= 720) {
+        direction = direction * -1;
+    } else if (options.bezierAngleX <= -720) {
+        direction = direction * -1;
+    }
+}
+
+// Draw a line from th17e center point towards the passed angle.
+function draw(angle, lineNumber) {
 
     // Progress the line along the offset.
-    ctx.lineDashOffset = -offset;
+    ctx.lineDashOffset = -offset + (lineNumber * options.lineDelay);
 
+    ctx.setLineDash([options.lineLength, options.lineGap]);
     ctx.lineWidth = options.lineWidth;
 
     ctx.beginPath();
-    x1 = canvas.width/2;
-    y1 = canvas.height/2;
-    length =  400;
-    
-    x2 = x1 + Math.cos(Math.PI * (angle + 180 - ((options.angleSize - 180) / 2)) / 180) * length;
-    y2 = y1 + Math.sin(Math.PI * (angle + 180 - ((options.angleSize - 180) / 2)) / 180) * length;
-    
+    x1 = canvas.width / 2 + options.startX;
+    y1 = canvas.height / 2 + options.startY;
+
+    x2 = x1 + Math.cos(Math.PI * (angle + 180 - ((options.angleSize - 180) / 2)) / 180) * options.totalLength;
+    y2 = y1 + Math.sin(Math.PI * (angle + 180 - ((options.angleSize - 180) / 2)) / 180) * options.totalLength;
+
     // ctx.moveTo(x1, y1);
     // ctx.lineTo(x2, y2);
     // ctx.stroke();
 
-    const bezierAngleX = 0;
-    const bezierAngleY = 0;
-
     // ctx.beginPath();
     ctx.moveTo(x1, y1);
-    ctx.bezierCurveTo(x1 + bezierAngleX, y1 + bezierAngleY, x2 + bezierAngleX, y2 + bezierAngleY, x2, y2);
+    ctx.bezierCurveTo(x1 + options.bezierAngleX, y1 + options.bezierAngleY, x2 + options.bezierAngleX, y2 + options.bezierAngleY, x2, y2);
+
+
     ctx.stroke();
+
 }
 
 //draw(180);
@@ -56,20 +92,30 @@ function draw(angle) {
 
 // // Draw lines based on option parameters.
 function animate() {
+    //progressAni();
 
     offset += parseInt(options.animationSpeed);
 
     // Wait for lines to be off screen before refreshing them.
-    if (offset > options.lineLength * 3) {
+    if (offset > options.lineLength + options.lineGap) {
         offset = 0;
     }
 
     // Clear canvas each frame.
-    ctx.clearRect(-canvas.width/2, -canvas.height/2, canvas.width * 2, canvas.height * 2);
+    ctx.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width * 2, canvas.height * 2);
 
     // Draw an amount of lines based on options.
-    for (let i = 0; i < options.lineCount; i ++) {
-        draw(options.angleSize / (options.lineCount - 1) * i);
+    for (let i = 0; i < options.lineCount; i++) {
+
+        // Skip drawing subtracted lines.
+
+        const rangeStart = Math.round(options.lineCount / 2) - Math.round(options.subtractedLines / 2);
+        const rangeEnd = rangeStart + options.subtractedLines;
+
+        if (i > rangeStart && i < rangeEnd) {
+            continue;
+        }
+        draw(options.angleSize / (options.lineCount - 1) * i, i);
     }
 
     // Refresh the animation ~60 times per second.
@@ -83,6 +129,31 @@ animate();
 const inputArray = Array.from(document.querySelectorAll('.option-input'));
 inputArray.forEach((input) => {
     input.addEventListener('input', () => {
-        options[input.id] = input.value;
+        options[input.id] = parseInt(input.value);
     });
-})
+});
+
+const randomize = () => {
+    Array.from(Object.keys(ranges)).forEach(key => {
+        const newValue = randomInRange(ranges[key][0], ranges[key][1]);
+        options[key] = newValue;
+        const input = document.getElementById(key);
+        input.value = newValue;
+    });
+}
+
+// Add event listener to randomize button.
+const randomizeBtn = document.getElementById('randomizeBtn');
+randomizeBtn.addEventListener('click', () => {
+    randomize();
+});
+
+// Randomize on space key being pressed.
+document.addEventListener("keydown", (event) => {
+    randomize();
+});
+
+function randomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
